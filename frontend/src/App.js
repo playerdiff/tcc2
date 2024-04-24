18e92784d17e875ab5b9ebaa38377393e425c764
Guilherme <guilherme.sinapse.ped@gmail.com>
2024-04-24 15:56:00 -0300

    Mudanças nos erros

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,8 @@
-// App.js
 import React from 'react';
 import './App.css';
 import CarroList from './pages/CarroList';
 import pages from './pages/pages';
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -12,13 +10,11 @@ function App() {
       <header className="App-header">
         <h1>A L U C A R</h1>
       </header>
-      <div className={classes.root}>        
+      <div className="App-content">
         <BrowserRouter>
-          <Routes>            
-
-            <Route path={pages.routes.CarroList} exact><CarroList /></Route>
-
-          </Routes>          
+          <Routes>
+            <Route path={pages.routes.CarroList} element={<CarroList />} />
+          </Routes>
         </BrowserRouter>
       </div>
     </div>
